ed038c4f4812ba8cf36f7521d2aa6a4627e6114e
Michael Klishin <michael@defprotocol.org>
2013-02-11 09:22:35 +0400

    Titan-specific element predicates

--- src/clojure/clojurewerkz/titanium/elements.clj
+++ src/clojure/clojurewerkz/titanium/elements.clj
@@ -1,7 +1,8 @@
 (ns clojurewerkz.titanium.elements
   (:refer-clojure :exclude [assoc! dissoc!])
   (:require [clojure.walk :as w])
-  (:import [com.tinkerpop.blueprints Element Vertex]))
+  (:import [com.tinkerpop.blueprints Element Vertex]
+           [com.thinkaurelius.titan.core TitanElement]))
 
 ;;
 ;; API
@@ -78,3 +79,26 @@
   (doseq [k (.getPropertyKeys e)]
     (.removeProperty e k))
   e)
+
+(defn new?
+  "Returns true if entity has been newly created, false otherwise"
+  [^TitanElement e]
+  (.isNew e))
+
+(defn loaded?
+  "Returns true if entity has been loaded and not yet modified in the current transaction,
+   false otherwise"
+  [^TitanElement e]
+  (.isLoaded e))
+
+(defn modified?
+  "Returns true if entity has been loaded and modified in the current transaction,
+   false otherwise"
+  [^TitanElement e]
+  (.isModified e))
+
+(defn removed?
+  "Returns true if entity has been deleted in the current transaction,
+   false otherwise"
+  [^TitanElement e]
+  (.isRemoved e))
