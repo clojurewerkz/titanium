87e5f52ae95c911523021d2d04f28dd2967d966c
Zack Maril <zack@zacharymaril.com>
2013-03-19 15:47:35 +0400

    All the tests that once were and still apply now pass.

--- test/clojurewerkz/titanium/edges_test.clj
+++ test/clojurewerkz/titanium/edges_test.clj
@@ -2,7 +2,7 @@
   (:require [clojurewerkz.titanium.graph    :as tg]
             [clojurewerkz.titanium.indexing :as ti]
             [clojurewerkz.titanium.vertices :as tv]
-            [clojurewerkz.titanium.edges    :as te])
+            [clojurewerkz.titanium.edges    :as ted])
   (:use clojure.test))
 
 
@@ -10,43 +10,43 @@
   (tg/open-in-memory-graph)
   (let [from-node (tv/create! {:url "http://clojurewerkz.org/"})
         to-node   (tv/create! {:url "http://clojurewerkz.org/about.html"})
-        created   (te/connect! from-node :links to-node)
-        fetched   (te/find-by-id  (te/id-of created))]
-    (is (= (te/id-of created) (te/id-of fetched)))))
+        created   (ted/connect! from-node :links to-node)
+        fetched   (ted/find-by-id  (ted/id-of created))]
+    (is (= (ted/id-of created) (ted/id-of fetched)))))
 
 (deftest test-creating-and-immediately-finding-a-relationship-without-properties-twice
   (tg/open-in-memory-graph)
   (let [from-node (tv/create! {:url "http://clojurewerkz.org/"})
         to-node   (tv/create! {:url "http://clojurewerkz.org/about.html"})
-        created   (te/connect! from-node :links to-node)
-        fetched1  (te/find-by-id  (te/id-of created))
-        fetched2  (te/find-by-id  (te/id-of created))]
-    (is (= (te/get created :url) (te/get fetched1 :url) (te/get fetched2 :url)))))
+        created   (ted/connect! from-node :links to-node)
+        fetched1  (ted/find-by-id  (ted/id-of created))
+        fetched2  (ted/find-by-id  (ted/id-of created))]
+    (is (= (ted/get created :url) (ted/get fetched1 :url) (ted/get fetched2 :url)))))
 
 (deftest  test-creating-and-immediately-finding-a-relationship-with-properties
   (tg/open-in-memory-graph)
   (let [from-node (tv/create! {:url "http://clojurewerkz.org/"})
         to-node   (tv/create! {:url "http://clojurewerkz.org/about.html"})
-        created   (te/connect! from-node :links to-node  {:since "08 Nov, 2011"})
-        fetched   (te/find-by-id  (te/id-of created))]
-    (is (= (:since (te/to-map fetched)) "08 Nov, 2011"))
-    (is (= (te/id-of created) (te/id-of fetched)))))
+        created   (ted/connect! from-node :links to-node  {:since "08 Nov, 2011"})
+        fetched   (ted/find-by-id  (ted/id-of created))]
+    (is (= (:since (ted/to-map fetched)) "08 Nov, 2011"))
+    (is (= (ted/id-of created) (ted/id-of fetched)))))
 
 (deftest  test-creating-and-immediately-deleting-a-relationship-with-properties
   (tg/open-in-memory-graph)
   (let [from-node (tv/create! {:url "http://clojurewerkz.org/"})
         to-node   (tv/create! {:url "http://clojurewerkz.org/about.html"})
-        created   (te/connect! from-node :links to-node  {:since "08 Nov, 2011"})
-        fetched   (te/find-by-id  (te/id-of created))]
-    (is (= created (te/find-by-id (te/id-of created))))
-    (te/delete! created)
-    (is (nil? (te/find-by-id (te/id-of created))))))
+        created   (ted/connect! from-node :links to-node  {:since "08 Nov, 2011"})
+        fetched   (ted/find-by-id  (ted/id-of created))]
+    (is (= created (ted/find-by-id (ted/id-of created))))
+    (ted/delete! created)
+    (is (nil? (ted/find-by-id (ted/id-of created))))))
 
 (deftest test-listing-all-relationships-of-a-kind
   (tg/open-in-memory-graph)
   (let [from-node (tv/create! {:url "http://clojurewerkz.org/"})
         to-node   (tv/create! {:url "http://clojurewerkz.org/about.html"})
-        created   (te/connect! from-node :links to-node  {:since "08 Nov, 2011"})
+        created   (ted/connect! from-node :links to-node  {:since "08 Nov, 2011"})
         rs1       (tv/all-edges-of from-node :links)
         rs2       (tv/all-edges-of to-node   :links)
         rs3       (tv/all-edges-of to-node   :knows)]
@@ -58,7 +58,7 @@
   (tg/open-in-memory-graph)
   (let [from-node (tv/create! {:url "http://clojurewerkz.org/"})
         to-node   (tv/create! {:url "http://clojurewerkz.org/about.html"})
-        created   (te/connect! from-node :links to-node  {:since "08 Nov, 2011"})
+        created   (ted/connect! from-node :links to-node  {:since "08 Nov, 2011"})
         rs1       (tv/incoming-edges-of to-node :links)
         rs2       (tv/incoming-edges-of to-node :linkes)
         rs3       (tv/all-edges-of to-node   :knows)]
@@ -69,7 +69,7 @@
   (tg/open-in-memory-graph)
   (let [from-node (tv/create! {:url "http://clojurewerkz.org/"})
         to-node   (tv/create! {:url "http://clojurewerkz.org/about.html"})
-        created   (te/connect! from-node :links to-node  {:since "08 Nov, 2011"})
+        created   (ted/connect! from-node :links to-node  {:since "08 Nov, 2011"})
         rs1       (tv/outgoing-edges-of from-node :links)
         rs2       (tv/outgoing-edges-of from-node :linkes)
         rs3       (tv/all-edges-of to-node   :knows)]
@@ -80,10 +80,10 @@
   (tg/open-in-memory-graph)
   (let [from-node (tv/create! {:url "http://clojurewerkz.org/"})
         to-node   (tv/create! {:url "http://clojurewerkz.org/about.html"})
-        edge      (te/connect! from-node :links to-node {:since "08 Nov, 2011"})
-        edge'     (te/assoc! edge :since "04 Nov, 2011")]
+        edge      (ted/connect! from-node :links to-node {:since "08 Nov, 2011"})
+        edge'     (ted/assoc! edge :since "04 Nov, 2011")]
     (is (= edge edge'))
-    (is (= (:since (te/to-map edge)) "04 Nov, 2011"))))
+    (is (= (:since (ted/to-map edge)) "04 Nov, 2011"))))
 
 ;; ;; TODO: add multiple edges at once
 ;; ;; TODO maybe-add-edge
@@ -93,19 +93,20 @@
 ;; ;; Edges
 ;; ;;
 
-;; (deftest test-getting-edge-head-and-tail
-;;   (let [g  (tg/open-in-memory-graph)
-;;         m1 {"station" "Boston Manor" "lines" #{"Piccadilly"}}
-;;         m2 {"station" "Northfields"  "lines" #{"Piccadilly"}}
-;;         v1 (tg/add-vertex g m1)
-;;         v2 (tg/add-vertex g m2)
-;;         e  (tg/add-edge g v1 v2 "links")]
-;;     (is (= :links (ted/label-of e)))
-;;     (is (= v2 (ted/head-vertex e)))
-;;     (is (= v1 (ted/tail-vertex e)))))
+(deftest test-getting-edge-head-and-tail
+  (tg/open-in-memory-graph)
+  (let [m1 {"station" "Boston Manor" "lines" #{"Piccadilly"}}
+        m2 {"station" "Northfields"  "lines" #{"Piccadilly"}}
+        v1 (tv/create! m1)
+        v2 (tv/create! m2)
+        e  (ted/connect! v1 :links v2)]
+    (is (= :links (ted/label-of e)))
+    (is (= v2 (ted/head-vertex e)))
+    (is (= v1 (ted/tail-vertex e)))))
 
 ;; #_ (deftest test-getting-edge-head-and-tail-via-fancy-macro
-;;      (let [g  (tg/open-in-memory-graph)
+;;      (tg/open-in-memory-graph)
+;;      (let [g
 ;;            m1 {"station" "Boston Manor" "lines" #{"Piccadilly"}}
 ;;            m2 {"station" "Northfields"  "lines" #{"Piccadilly"}}]
 ;;        (tg/populate g
