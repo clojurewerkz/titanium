660fa8989969949a2affcea425f8095d8127225b
Zack Maril <zack@zacharymaril.com>
2013-03-19 16:34:01 +0400

    Persistent vertices test

--- test/clojurewerkz/titanium/vertices_test.clj
+++ test/clojurewerkz/titanium/vertices_test.clj
@@ -4,7 +4,8 @@
             [clojurewerkz.titanium.edges    :as ted]            
             [clojurewerkz.titanium.indexing :as ti]
             [clojurewerkz.titanium.types    :as tt])
-  (:use clojure.test))
+  (:use clojure.test
+        [clojurewerkz.titanium.conf :only (clear-db conf)]))
 
 ;;
 ;; Vertices
@@ -183,3 +184,106 @@
         v'       (tv/clear! v)]
     (is (= v v'))
     (is (= {} (dissoc (tv/to-map v) :__id__)))))
+
+
+(deftest persistent-vertex-test
+  (clear-db)
+  (tg/open conf)
+  (tg/transact!    
+   (tt/create-vertex-key-once :vname String {:indexed true})
+   (tt/create-vertex-key-once :age Long {:indexed true})
+   (tt/create-vertex-key-once :first-name String {:indexed true})
+   (tt/create-vertex-key-once :last-name String {:indexed true}))
+  
+  (testing "Deletion of vertices"    
+    (tg/transact!
+     (let [u (tv/create! {:vname "uniquename"})
+           u-id (tv/id-of u)]
+       (tv/delete! u)
+       (is (=  nil (tv/find-by-id u-id)))
+       (is (empty? (tv/find-by-kv :vname "uniquename"))))))
+  
+  (testing "Simple property mutation" 
+    (tg/transact!
+     (let [u (tv/create! {:vname "v1" :a 1 :b 1})]
+       (tv/assoc! u :b 2)
+       (tv/dissoc! u :a)
+       (is (= 2   (tv/get u :b)))
+       (is (= nil (tv/get u :a))))))
+
+  (testing "Multiple property mutation"
+    (tg/transact!
+     (let [u (tv/create! {:vname "v1" :a 0 :b 2})]
+       (tv/merge! u {:a 1 :b 2 :c 3})
+       (is (= 1   (tv/get u :a)))
+       (is (= 2   (tv/get u :b)))
+       (is (= 3   (tv/get u :c))))))
+
+  (testing "Property map"
+    (tg/transact!
+     (let [v1 (tv/create! {:vname "v1" :a 1 :b 2 :c 3})
+           prop-map (tv/to-map v1)]
+       (is (= 1 (prop-map :a)))
+       (is (= 2 (prop-map :b)))
+       (is (= 3 (prop-map :c))))))
+
+  (testing "Find by single id"
+    (tg/transact!
+     (let [v1 (tv/create! {:prop 1})
+           v1-id (tv/id-of v1)
+           v1-maybe (tv/find-by-id v1-id)]
+       (is (= 1 (tv/get v1-maybe :prop))))))
+
+  (testing "Find by multiple ids"
+    (tg/transact!
+     (let [v1 (tv/create! {:prop 1})
+           v2 (tv/create! {:prop 2})
+           v3 (tv/create! {:prop 3})
+           ids (map tv/id-of [v1 v2 v3])
+           v-maybes (apply tv/find-by-id ids)]
+       (is (= (range 1 4) (map #(tv/get % :prop) v-maybes))))))
+
+  (testing "Find by kv"
+    (tg/transact!
+     (let [v1 (tv/create! {:age 1 :vname "A"})
+           v2 (tv/create! {:age 2 :vname "B"})
+           v3 (tv/create! {:age 2 :vname "C"})]
+       (is (= #{"A"}
+              (set (map #(tv/get % :vname) (tv/find-by-kv :age 1)))))
+       (is (= #{"B" "C"}
+              (set (map #(tv/get % :vname) (tv/find-by-kv :age 2))))))))
+
+  (testing "Upsert!"
+    (tg/transact!
+     (let [v1-a (tv/upsert! :first-name
+                           {:first-name "Zack" :last-name "Maril" :age 21})
+           v1-b (tv/upsert! :first-name
+                           {:first-name "Zack" :last-name "Maril" :age 22})
+           v2   (tv/upsert! :first-name
+                           {:first-name "Brooke" :last-name "Maril" :age 19})]
+       (is (= 22
+              (tv/get (tv/refresh (first v1-a)) :age)
+              (tv/get (tv/refresh (first v1-b)) :age)))
+       (tv/upsert! :last-name {:last-name "Maril"
+                              :heritage "Some German Folks"})
+       (is (= "Some German Folks"
+              (tv/get (tv/refresh (first v1-a)) :heritage)
+              (tv/get (tv/refresh (first v1-b)) :heritage)
+              (tv/get (tv/refresh (first v2)) :heritage))))))
+
+  (testing "Unique upsert!"
+    (tg/transact!
+     (let [v1-a (tv/unique-upsert! :first-name
+                                  {:first-name "Zack" :last-name "Maril" :age 21})
+           v1-b (tv/unique-upsert! :first-name
+                                  {:first-name "Zack" :last-name "Maril" :age 22})
+           v2   (tv/unique-upsert! :first-name
+                                  {:first-name "Brooke" :last-name "Maril" :age 19})]
+       (is (= 22
+              (tv/get (tv/refresh v1-a) :age)
+              (tv/get (tv/refresh v1-b) :age)))       
+       (is (thrown? Throwable #"There were 2 vertices returned."
+                    (tv/unique-upsert! :last-name {:last-name "Maril"}))))))
+  
+  (tg/shutdown)
+  (clear-db))
