674390ea73e931343cacf7302107ddf99040c92e
Michael Klishin <michael@clojurewerkz.org>
2014-02-13 09:43:14 +0400

    Ditto

--- src/clojure/clojurewerkz/titanium/indexing.clj
+++ src/clojure/clojurewerkz/titanium/indexing.clj
@@ -1,3 +1,13 @@
+;; Copyright (c) 2013-2014 Michael S. Klishin, Alex Petrov, Zack Maril, and The ClojureWerkz
+;; Team
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
 (ns clojurewerkz.titanium.indexing
   (:refer-clojure :exclude [get remove count])
   (:import [com.tinkerpop.blueprints Graph Index Element]))
