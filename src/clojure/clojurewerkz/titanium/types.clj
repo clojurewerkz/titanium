1a49d5091dbac8bed3f5e1c0580fe074ac40101d
Ray Miller <ray@1729.org.uk>
2014-06-07 18:47:58 +0100

    Passing tests for types.

--- src/clojure/clojurewerkz/titanium/types.clj
+++ src/clojure/clojurewerkz/titanium/types.clj
@@ -9,13 +9,9 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns clojurewerkz.titanium.types
-  (:import (com.thinkaurelius.titan.core TitanType KeyMaker LabelMaker TypeMaker$UniquenessConsistency)
-           (com.tinkerpop.blueprints Vertex Edge Direction Graph)))
-
-;; Types are uniquely identified by their name. Attempting to define a
-;; new type with an existing name will result in an exception. Note,
-;; that labels and keys share the same namespace, i.e., labels and keys
-;; cannot have the same name either.
+  (:import (com.thinkaurelius.titan.core TitanGraph TitanType KeyMaker LabelMaker Order
+                                         Parameter TypeMaker$UniquenessConsistency)
+           (com.tinkerpop.blueprints Vertex Edge Graph)))
 
 (defn get-type
   "In Titan, edge labels and property keys are types which can be
@@ -23,7 +19,7 @@
    efficiency, and higher performance. Types are uniquely identified by
    their name and are themselves vertices in the graph. Type vertices
    can be retrieved by their name."
-  [graph tname]
+  [^TitanGraph graph tname]
   (.getType graph (name tname)))
 
 (defn- convert-bool-to-lock
@@ -32,47 +28,111 @@
     TypeMaker$UniquenessConsistency/LOCK
     TypeMaker$UniquenessConsistency/NO_LOCK))
 
-(defn unique-direction-converter
-  [type-maker unique-direction unique-locked]
-  (when unique-direction
-    (when (#{:both :in} unique-direction)
-      (.unique type-maker
-               Direction/IN
-               (convert-bool-to-lock unique-locked)))
-    (when (#{:both :out} unique-direction)
-      (.unique type-maker
-               Direction/OUT
-               (convert-bool-to-lock unique-locked)))))
+(defn- convert-keyword-to-sort-order
+  [kw]
+  (case kw
+    :asc  Order/ASC
+    :desc Order/DESC))
+
+(defn- ensure-collection
+  "If `x` is a collecion, return that, otherwise return a single-element list
+   containing `x`."
+  [x]
+  (if (coll? x) x (list x)))
 
 (defn deflabel
-  "Creates a edge label with the given properties."
+  "Creates an edge label with the given properties, identified by `tname`.
+   Attempting to define a new type with an existing name will result in
+   an exception. Note that labels and keys share the same namespace,
+   i.e., labels and keys cannot have the same name either. Use
+   `deflabel-once` to check for existence of the named property before
+   attempting to create it.
+
+   Options:
+
+     :unidirected? (default false)
+
+     The default is for edges to be directed. Specifying `:unidirected
+     true` configures this label to be uni-directed; this means that the
+     edge is only created in the out-going direction. One can think of
+     uni-directed edges as links pointing to another vertex such that
+     only the outgoing vertex but not the incoming vertex is aware of
+     its existence.
+
+     :cardinality = :many-to-many (default) | :one-to-many | :one-to-one | :many-to-one
+
+     A :one-to-many label allows at most one incoming edge of this label
+     for each vertex in the graph. For instance, the label “fatherOf” is
+     biologically a oneToMany edge label.
+
+     A :many-to-one label allows at most one outgoing edge of this label
+     for each vertex in the graph. For instance, the label “sonOf” is
+     biologically a manyToOne edge label.
+
+     A :one-to-one label allows at most one outgoing and one incoming
+     edge of this label for each vertex in the graph
+
+     When a :one-to-many, :many-to-one or :one-to-one cardinality is specified,
+     we default to using locking to ensure the uniqueness constraint. You can
+     override this by specifying `:unique-locked? false`, in which case concurrent
+     transactions may overwrite existing uniqueness constraints.
+
+     :sort-key = type-name | [type-name ...]
+
+     Specifying the sort key of a label allows edges with this label to
+     be efficiently retrieved in the specified sort-order.
+
+     :sort-order = :asc (default) | :desc
+
+     Defines in which order to sort the relations for efficient
+     retrieval, i.e. either increasing (:asc) or decreasing (:desc). This option has no
+     effect unless :sort-key is specified.
+
+     :signature = type-name | [type-name ...]
+
+     Specifying the signature of a type tells the graph database to
+     expect that relations of this type always have or are likely to
+     have an incident property or unidirected edge of the type included
+     in the signature. This allows the graph database to store such
+     edges more compactly and retrieve them more quickly. The signature
+     should not contain any types already included in the sort key. The
+     sort key provides the same storage and retrieval efficiency."
   ([graph tname] (deflabel graph tname {}))
-  ([graph tname {:keys [simple direction primary-key signature
-                        unique-direction unique-locked group]
-           :or {direction "directed"
-                primary-key nil
-                signature   nil
-                unique-direction false
-                unique-locked    true
-                group       default-group}}]
-     (let [type-maker (.. graph
-                          makeType
-                          (name (name tname))
-                          (group group))]
-       (unique-direction-converter type-maker unique-direction unique-locked)
-       (case direction
-         "directed"    (.directed type-maker)
-         "unidirected" (.unidirected type-maker))
-       (when signature (.signature type-maker signature))
-       (when primary-key (.primaryKey type-maker (into-array TitanType primary-key)))
-       (.makeEdgeLabel type-maker))))
+  ([^TitanGraph graph tname {:keys [unidirected? cardinality sort-key sort-order
+                                    signature unique-locked?]
+                             :or {unidirected?   false
+                                  cardinality    :many-to-many
+                                  sort-order     :asc
+                                  unique-locked? true}}]
+     {:pre [(#{:many-to-many :many-to-one :one-to-one :one-to-many} cardinality)
+            (#{:asc :desc} sort-order)]}
+     (let [^LabelMaker label-maker (.makeLabel graph (name tname))
+           ^TypeMaker$UniquenessConsistency consistency (convert-bool-to-lock unique-locked?)]
+       (case cardinality
+         :one-to-many  (.oneToMany label-maker consistency)
+         :one-to-one   (.oneToOne  label-maker consistency)
+         :many-to-one  (.manyToOne label-maker consistency)
+         :many-to-many (.manyToMany label-maker))
+       (when unidirected?
+         (.unidirected label-maker))
+       (when sort-key
+         (.sortKey label-maker
+                   (into-array TitanType (map (partial get-type graph)
+                                              (ensure-collection sort-key))))
+         (.sortOrder label-maker (convert-keyword-to-sort-order sort-order)))
+       (when signature
+         (.signature label-maker
+                     (into-array TitanType (map (partial get-type graph)
+                                                (ensure-collection signature)))))
+       (.make label-maker))))
 
 (defn- parse-index-specification
   [spec include-standard-index?]
   (let [indices (cond
                  (coll? spec) (into #{} (map name spec))
                  (true? spec) (conj #{} "standard")
-                 :else        (conj #{} (name spec)))]
+                 spec         (conj #{} (name spec))
+                 :else        #{})]
     (if include-standard-index?
       (conj indices "standard")
       indices)))
@@ -111,19 +171,22 @@
    Specifying `:unique? true` will automatically add the property to the standard
    index and will, by default, use a lock to ensure the uniqueness constraint. You can
    suppress locking by specifying `:unique-locked? false`, in which case concurrent
-   transactions may overwrite existing uniqueness constraints."
+   transactions may overwrite existing uniqueness constraints.
+
+   Note that we do not currently expose `list` types (which are only applicable to
+   vertices) - each vertex or edge can have at most one value for a given key type."
   ([graph tname data-type] (defkey graph tname data-type {}))
-  ([graph tname data-type {:keys [vertex-index edge-index unique? unique-locked?]
-                           :or   {unique? false, unique-locked? true}}]
-     (let [^KeyMaker type-maker (.makeType graph (name tname))]
-       (.dataType type-maker data-type)
+  ([^TitanGraph graph tname data-type {:keys [vertex-index edge-index unique? unique-locked?]
+                                       :or   {unique? false, unique-locked? true}}]
+     (let [^KeyMaker key-maker (.makeKey graph (name tname))]
+       (.dataType key-maker data-type)
        (doseq [index-name (parse-index-specification vertex-index unique?)]
-         (.indexed type-maker index-name Vertex))
+         (.indexed key-maker index-name Vertex (make-array Parameter 0)))
        (doseq [index-name (parse-index-specification edge-index false)]
-         (.indexed type-maker index-name Edge))
+         (.indexed key-maker index-name Edge (make-array Parameter 0)))
        (when unique?
-         (.unique type-maker (convert-bool-to-lock unique-locked?)))
-       (.make type-maker))))
+         (.unique key-maker (convert-bool-to-lock unique-locked?)))
+       (.make key-maker))))
 
 (defn deflabel-once
   "Checks to see if a edge label with the given name exists already.
--- test/clojurewerkz/titanium/types_test.clj
+++ test/clojurewerkz/titanium/types_test.clj
@@ -1,183 +1,238 @@
 (ns clojurewerkz.titanium.types-test
   (:import (com.tinkerpop.blueprints Vertex Edge Direction Graph))
   (:use [clojure.test]
-        [clojurewerkz.titanium.conf :only (clear-db conf)])
+        [clojurewerkz.titanium.test.conf :only (clear-db conf)])
   (:require [clojurewerkz.titanium.types :as tt]
             [clojurewerkz.titanium.vertices :as tv]
+            [clojurewerkz.titanium.edges :as te]
             [clojurewerkz.titanium.graph :as tg]))
 
 (deftest test-types
   (clear-db)
-  (tg/open conf)
 
-  (testing "Create group."
-    (tg/transact!
-     (let [my-group-name "My-Group-Name"
-           my-group (tt/defgroup 100 my-group-name)
-           default-group-id (.getID tt/default-group)]
-       (is (= my-group-name (.getName my-group))
-           "The group has the correct name")
-       (is (= 100 (.getID my-group))
-           "The group has the correct ID")
-       (is (not (= default-group-id (.getID my-group)))
-           "my-group has a different ID from the default group"))))
+  (let [graph (tg/open conf)]
+    (testing "Create property key."
 
-  (testing "Create property key."
-    (testing "With no parameters."
-      (tg/transact!
-       (tt/defkey :first-key Integer)
-       (let [k (tt/get-type :first-key)]
-         (is (.isPropertyKey k))
-         (is (not (.isEdgeLabel k)))
-         (is (= "first-key" (.getName k)))
-         (is (not (.hasIndex k "first-key" Vertex)))
-         (is (not (.hasIndex k "first-key" Edge)))
-         (is (not (.isUnique k Direction/OUT)))
-         (is (not (.isUnique k Direction/IN))))))
+      (testing "With no parameters."
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :first-key Integer)
+          (let [k (tt/get-type tx :first-key)]
+            (is (= (.getDataType k) Integer))
+            (is (.isPropertyKey k))
+            (is (not (.isEdgeLabel k)))
+            (is (= "first-key" (.getName k)))
+            (is (not (.hasIndex k "standard" Vertex)))
+            (is (not (.hasIndex k "standard" Edge)))
+            (is (.isUnique k Direction/OUT))
+            (is (not (.isUnique k Direction/IN))))))
 
-    (testing "With indexed vertex."
-      (tg/transact!    
-       (tt/defkey :second-key Integer
-                               {:indexed-vertex? true})
-       (let [k (tt/get-type :second-key)]
-         (is (.isPropertyKey k))
-         (is (not (.isEdgeLabel k)))
-         (is (= "second-key" (.getName k)))
-         (is (.hasIndex k "standard" Vertex))
-         (is (not (.hasIndex k "search" Edge)))
-         (is (not (.isUnique k Direction/OUT)))
-         (is (not (.isUnique k Direction/IN))))))
+      (testing "With indexed vertex."
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :second-key Integer {:vertex-index true})
+          (let [k (tt/get-type tx :second-key)]
+            (is (.isPropertyKey k))
+            (is (not (.isEdgeLabel k)))
+            (is (= "second-key" (.getName k)))
+            (is (.hasIndex k "standard" Vertex))
+            (is (not (.hasIndex k "standard" Edge)))
+            (is (not (.hasIndex k "search" Vertex)))
+            (is (.isUnique k Direction/OUT))
+            (is (not (.isUnique k Direction/IN))))))
 
-    (testing "With indexed vertex."
-      (tg/transact! 
-       (tt/defkey :third-key Integer
-                               {:indexed-vertex? true
-                                :unique-direction :out})
-       (let [k (tt/get-type :third-key)]
-         (is (.isPropertyKey k))
-         (is (not (.isEdgeLabel k)))
-         (is (= "third-key" (.getName k)))
-         (is (.hasIndex k "standard" Vertex))
-         (is (not (.hasIndex k "standard" Edge)))
-         (is  (.isUnique k Direction/OUT))
-         (is (not (.isUnique k Direction/IN))))))
+      (testing "With indexed-unique vertex."
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :third-key Integer {:unique? true})
+          (let [k (tt/get-type tx :third-key)]
+            (is (.isPropertyKey k))
+            (is (not (.isEdgeLabel k)))
+            (is (= "third-key" (.getName k)))
+            (is (.hasIndex k "standard" Vertex))
+            (is (not (.hasIndex k "standard" Edge)))
+            (is (.isUnique k Direction/OUT))
+            (is (.isUnique k Direction/IN)))))
 
-    (testing "With indexed edge."
-      (tg/transact! 
-       (tt/defkey :fourth-key Integer
-                               {:indexed-edge? true
-                                :unique-direction :out})
-       (let [k (tt/get-type :fourth-key)]
-         (is (.isPropertyKey k) )
-         (is (not (.isEdgeLabel k)))
-         (is (= "fourth-key" (.getName k)))
-         (is (not (.hasIndex k "standard" Vertex)))
-         (is (.hasIndex k "standard" Edge))
-         (is (.isUnique k Direction/OUT))
-         (is (not (.isUnique k Direction/IN))))))
+      (testing "With indexed edge."
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :fourth-key Integer {:edge-index true})
+          (let [k (tt/get-type tx :fourth-key)]
+            (is (.isPropertyKey k) )
+            (is (not (.isEdgeLabel k)))
+            (is (= "fourth-key" (.getName k)))
+            (is (not (.hasIndex k "standard" Vertex)))
+            (is (.hasIndex k "standard" Edge))
+            (is (.isUnique k Direction/OUT))
+            (is (not (.isUnique k Direction/IN))))))
 
-    (testing "With searchable vertex."
-      (tg/transact! 
-       (tt/defkey :fifth-key Integer
-                               {:indexed-vertex? true
-                                :searchable? true
-                                :unique-direction :out})
-       (let [k (tt/get-type :fifth-key)]
-         (is (.isPropertyKey k))
-         (is (not (.isEdgeLabel k)))
-         (is (= "fifth-key" (.getName k)) )
-         (is (not (.hasIndex k "standard" Vertex)))
-         (is (.hasIndex k "search" Vertex))
-         (is (not (.hasIndex k "standard" Edge)))
-         (is (not (.hasIndex k "search" Edge)))
-         (is (.isUnique k Direction/OUT))
-         (is (not (.isUnique k Direction/IN))))))
-    
-    (testing "With searchable edge."
-      (tg/transact! 
-       (tt/defkey :sixth-key Integer
-                               {:indexed-edge? true
-                                :searchable? true
-                                :unique-direction :out})
-       (let [k (tt/get-type :sixth-key)]
-         (is (.isPropertyKey k))
-         (is (not (.isEdgeLabel k)))
-         (is (= "sixth-key" (.getName k)) )
-         (is (not (.hasIndex k "standard" Vertex)))
-         (is (not (.hasIndex k "search" Vertex)))
-         (is (not (.hasIndex k "standard" Edge)))
-         (is (.hasIndex k "search" Edge))
-         (is (.isUnique k Direction/OUT))
-         (is (not (.isUnique k Direction/IN))))))
+      (testing "With searchable vertex."
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :fifth-key Integer {:vertex-index "search"})
+          (let [k (tt/get-type tx :fifth-key)]
+            (is (.isPropertyKey k))
+            (is (not (.isEdgeLabel k)))
+            (is (= "fifth-key" (.getName k)) )
+            (is (not (.hasIndex k "standard" Vertex)))
+            (is (.hasIndex k "search" Vertex))
+            (is (not (.hasIndex k "standard" Edge)))
+            (is (not (.hasIndex k "search" Edge)))
+            (is (.isUnique k Direction/OUT))
+            (is (not (.isUnique k Direction/IN))))))
 
-    (testing "Unique property in both directions."
-      (tg/transact! 
-       (tt/defkey :seventh-key Long
-                               {:indexed-vertex? true
-                                :indexed-edge? true
-                                :unique-direction :both})
-       (let [k (tt/get-type :seventh-key)]
-         (tv/create! {:seventh-key 1})
-         (is (.isPropertyKey k))
-         (is (not (.isEdgeLabel k)))
-         (is (= "seventh-key" (.getName k)))
-         (is (.hasIndex k "standard" Vertex))
-         (is (not (.hasIndex k "search" Vertex)))
-         (is (.hasIndex k "standard" Edge))
-         (is (not (.hasIndex k "search" Edge)))
-         (is (.isUnique k Direction/OUT))
-         (is (.isUnique k Direction/IN))
-         (is (thrown? java.lang.IllegalArgumentException
-                      (tv/create! {:seventh-key 1}))))))
+      (testing "With searchable edge."
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :sixth-key Integer {:edge-index "search"})
+          (let [k (tt/get-type tx :sixth-key)]
+            (is (.isPropertyKey k))
+            (is (not (.isEdgeLabel k)))
+            (is (= "sixth-key" (.getName k)) )
+            (is (not (.hasIndex k "standard" Vertex)))
+            (is (not (.hasIndex k "search" Vertex)))
+            (is (not (.hasIndex k "standard" Edge)))
+            (is (.hasIndex k "search" Edge))
+            (is (.isUnique k Direction/OUT))
+            (is (not (.isUnique k Direction/IN))))))
 
-    (testing "Search all the things."
-      (tg/transact! 
-       (tt/defkey :eighth-key Integer
-                               {:indexed-edge? true
-                                :indexed-vertex? true
-                                :searchable? true
-                                :unique-direction :out})
-       (let [k (tt/get-type :eighth-key)]
-         (is (.isPropertyKey k))
-         (is (not (.isEdgeLabel k)))
-         (is (= "eighth-key" (.getName k)) )
-         (is (not (.hasIndex k "standard" Vertex)))
-         (is (.hasIndex k "search" Vertex))
-         (is (not (.hasIndex k "standard" Edge)))
-         (is (.hasIndex k "search" Edge))
-         (is (.isUnique k Direction/OUT))
-         (is (not (.isUnique k Direction/IN)))))))
+      (testing "With unique indexed vertex and edge. (Note: uniqueness only applies to vertex.)"
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :seventh-key Long {:vertex-index true :edge-index true :unique? true})
+          (let [k (tt/get-type tx :seventh-key)]
+            (tv/create! tx {:seventh-key 1})
+            (is (.isPropertyKey k))
+            (is (not (.isEdgeLabel k)))
+            (is (= "seventh-key" (.getName k)))
+            (is (.hasIndex k "standard" Vertex))
+            (is (not (.hasIndex k "search" Vertex)))
+            (is (.hasIndex k "standard" Edge))
+            (is (not (.hasIndex k "search" Edge)))
+            (is (.isUnique k Direction/OUT))
+            (is (.isUnique k Direction/IN))
+            (is (thrown? java.lang.IllegalArgumentException
+                         (tv/create! tx {:seventh-key 1}))))))
 
-  ;;TODO add in tests for unique-direction in
-  ;;TODO add in tests for unique-locking
+      (testing "Search all the things."
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :eighth-key Integer {:edge-index "search" :vertex-index "search" :unique? true})
+          (let [k (tt/get-type tx :eighth-key)]
+            (is (.isPropertyKey k))
+            (is (not (.isEdgeLabel k)))
+            (is (= "eighth-key" (.getName k)))
+            (is (.hasIndex k "standard" Vertex)) ;; Required for unique constraint
+            (is (.hasIndex k "search" Vertex))
+            (is (not (.hasIndex k "standard" Edge)))
+            (is (.hasIndex k "search" Edge))
+            (is (.isUnique k Direction/OUT))
+            (is (.isUnique k Direction/IN)))))
 
-  (testing "Create edge labels."
-    (testing "With no parameters"
-      (tg/transact!
-       (tt/deflabel :first-label)
-       (let [lab (tt/get-type :first-label)]
-         (is (.isEdgeLabel lab))
-         (is (not (.isPropertyKey lab)))
-         (is (= "first-label" (.getName lab)))
-         (is (.isDirected lab))
-         (is (not (.isUnidirected lab)))
-         (is (not (.isUnique lab Direction/IN)))
-         (is (not (.isUnique lab Direction/OUT)))
-         (is (= tt/default-group (.getGroup lab)) "the label has the default group"))))
+      (testing "Unique vertex, no unique locking."
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :ninth-key Integer {:unique? true :unique-locked? false})
+          (let [k (tt/get-type tx :ninth-key)]
+            (is (.isPropertyKey k))
+            (is (not (.isEdgeLabel k)))
+            (is (= "ninth-key" (.getName k)))
+            (is (.hasIndex k "standard" Vertex)) ;; Required for unique constraint
+            (is (not (.hasIndex k "search" Vertex)))
+            (is (not (.hasIndex k "standard" Edge)))
+            (is (not (.hasIndex k "search" Edge)))
+            (is (.isUnique k Direction/OUT))
+            (is (.isUnique k Direction/IN)))))
 
-    (testing "Unidirected, nondefault group, unique direction."
-      (tg/transact!
-       (let [test-group (tt/defgroup 60 "test")
-             label (tt/deflabel :second-label {:direction "unidirected"
-                                                        :group test-group
-                                                        :unique-direction :out})
-             lab (tt/get-type :second-label)]
-         (is (.isEdgeLabel lab))
-         (is (not (.isPropertyKey lab)))
-         (is (= "second-label" (.getName lab)))
-         (is (not (.isDirected lab)))
-         (is (.isUnidirected lab))
-         (is (not (.isUnique lab Direction/IN)))
-         (is (.isUnique lab Direction/OUT))
-         (is (= test-group (.getGroup lab)) "the label has the default group")))))
-  (tg/shutdown))
+      (testing "Attemp to re-use key name throws exception"
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :tenth-key Integer)
+          (is (thrown-with-msg? java.lang.IllegalArgumentException
+                                #"The given value .* is already used as a property"
+                                (tt/defkey tx :tenth-key Integer))))))
+
+    (testing "Create edge labels."
+
+      (testing "With no parameters"
+        (tg/with-transaction [tx graph]
+         (tt/deflabel tx :first-label)
+         (let [lab (tt/get-type tx :first-label)]
+           (is (.isEdgeLabel lab))
+           (is (not (.isPropertyKey lab)))
+           (is (= "first-label" (.getName lab)))
+           (is (.isDirected lab))
+           (is (not (.isUnidirected lab)))
+           (is (not (.isUnique lab Direction/IN)))
+           (is (not (.isUnique lab Direction/OUT))))))
+
+      ;; An edge lable is out-unique, if a vertex has at most one
+      ;; outgoing edge for that label. For edges, out- and in-uniqueness
+      ;; translate to cardinality constraints. Father is an exmaple of a
+      ;; functional edge label, since each person has at most one
+      ;; father.
+
+      (testing "Cardinality one-to-many"
+        (tg/with-transaction [tx graph]
+          (tt/deflabel tx :second-label {:cardinality :one-to-many})
+          (let [lab (tt/get-type tx :second-label)]
+            (is (.isEdgeLabel lab))
+            (is (not (.isPropertyKey lab)))
+            (is (= "second-label" (.getName lab)))
+            (is (.isDirected lab))
+            (is (not (.isUnidirected lab)))
+            (is (.isUnique lab Direction/IN))
+            (is (not (.isUnique lab Direction/OUT))))))
+
+      (testing "Cardinality many-to-one"
+        (tg/with-transaction [tx graph]
+         (tt/deflabel tx :third-label {:cardinality :many-to-one})
+         (let [lab (tt/get-type tx :third-label)]
+           (is (.isEdgeLabel lab))
+           (is (not (.isPropertyKey lab)))
+           (is (= "third-label" (.getName lab)))
+           (is (.isDirected lab))
+           (is (not (.isUnidirected lab)))
+           (is (not (.isUnique lab Direction/IN)))
+           (is (.isUnique lab Direction/OUT)))))
+
+      (testing "Cardinality one-to-one"
+        (tg/with-transaction [tx graph]
+          (tt/deflabel tx :fourth-label {:cardinality :one-to-one})
+          (let [lab (tt/get-type tx :fourth-label)]
+            (is (.isEdgeLabel lab))
+            (is (not (.isPropertyKey lab)))
+            (is (= "fourth-label" (.getName lab)))
+            (is (.isDirected lab))
+            (is (not (.isUnidirected lab)))
+            (is (.isUnique lab Direction/IN))
+            (is (.isUnique lab Direction/OUT)))))
+
+      (testing "Sort key"
+        (tg/with-transaction [tx graph]
+          (tt/defkey tx :rank Integer)
+          (tt/deflabel tx :fifth-label {:sort-key :rank})
+          (tt/deflabel tx :fifth-label-r {:sort-key :rank, :sort-order :desc})
+          (let [lab (tt/get-type tx :fifth-label)]
+            (is (.isEdgeLabel lab))
+            (is (not (.isPropertyKey lab)))
+            (is (= "fifth-label" (.getName lab)))
+            (is (.isDirected lab))
+            (is (not (.isUnidirected lab)))
+            (is (not (.isUnique lab Direction/IN)))
+            (is (not (.isUnique lab Direction/OUT))))
+          (let [v1 (tv/create! tx {:name "v1" :rank 3})
+                v2 (tv/create! tx {:name "v2" :rank 2})
+                v3 (tv/create! tx {:name "v3" :rank 1})]
+            (te/connect! tx v1 :fifth-label v2)
+            (te/connect! tx v1 :fifth-label v3)
+            (te/connect! tx v1 :fifth-label-r v2)
+            (te/connect! tx v1 :fifth-label-r v3)
+            (let [vs (seq (tv/connected-out-vertices v1 :fifth-label))]
+              (is (= (count vs) 2))
+              (is (= (tv/get (first vs) :name)) "v2")
+              (is (= (tv/get (second vs) :name)) "v3"))
+            (let [vs (seq (tv/connected-out-vertices v1 :fifth-label-r))]
+              (is (= (count vs) 2))
+              (is (= (tv/get (first vs) :name)) "v3")
+              (is (= (tv/get (second vs) :name)) "v2")))))
+
+      ;; TODO
+      ;; Test composite sort-key
+      ;; Test signature
+      ;; Test unique-locked? false
+
+      )
+
+    (tg/shutdown graph)))
