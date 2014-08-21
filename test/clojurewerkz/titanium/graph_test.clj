6c276fb2dd9153097bf55b81badfe252e3d837f5
Ray Miller <ray@1729.org.uk>
2014-08-21 12:55:28 +0100

    Remove duelling transactions test (it seems to depend on a race condidion). Add some generic graph schema, population and query tests.

--- test/clojurewerkz/titanium/graph_test.clj
+++ test/clojurewerkz/titanium/graph_test.clj
@@ -2,30 +2,28 @@
   (:require [clojurewerkz.titanium.graph    :as tg]
             [clojurewerkz.titanium.vertices :as tv]
             [clojurewerkz.titanium.edges    :as ted]
-            [clojurewerkz.titanium.types    :as tt]
+            [clojurewerkz.titanium.schema   :as ts]
             [clojurewerkz.support.io        :as sio]
             [clojurewerkz.archimedes.graph  :as c])
   (:use clojure.test
         [clojurewerkz.titanium.test.support :only (graph-fixture *graph*)])
-  (:import java.io.File
+  (:import org.apache.commons.io.FileUtils
            (com.thinkaurelius.titan.graphdb.vertices StandardVertex)
            (com.thinkaurelius.titan.graphdb.database StandardTitanGraph)
            [java.util.concurrent CountDownLatch TimeUnit]))
 
 (use-fixtures :once graph-fixture)
 
-(deftest open-and-close-a-local-graph-with-a-directory-path
-  (let [d (doto (sio/create-temp-dir) (.deleteOnExit))
-        graph (tg/open d)]
-    (is graph)
-    (is (nil? (tg/shutdown graph)))))
+(deftest open-and-close-a-local-graph-with-a-shortcut
+  (let [d (sio/create-temp-dir)]
+    (is (let [graph (tg/open (str "berkeleyje:" (.getPath d)))]
+          (and graph (nil? (tg/shutdown graph)))))))
 
 (deftest test-open-and-close-a-local-graph-with-a-connfiguration-map
-  (let [d (doto (sio/create-temp-dir) (.deleteOnExit))
-        graph  (tg/open {"storage.directory" (.getPath d)
-                         "storage.backend"  "berkeleyje"})]
-    (is graph)
-    (is (nil? (tg/shutdown graph)))))
+  (let [d (sio/create-temp-dir)]
+    (is (let [graph  (tg/open {"storage.directory" (.getPath d)
+                               "storage.backend"  "berkeleyje"})]
+          (and graph (nil? (tg/shutdown graph)))))))
 
 (deftest test-conf-graph
   (testing "Graph type"
@@ -34,49 +32,64 @@
   (testing "Vertex type"
     (tg/with-transaction [tx *graph*]
         (let [vertex (tv/create! tx)]
-          (is (= StandardVertex (type vertex))))))
+          (is (= StandardVertex (type vertex)))))))
 
-  (testing "Dueling transactions"
+(deftest graph-of-the-gods
+
+  (testing "Configure edge and vertex properties"
+    (ts/with-management-system [mgmt *graph*]
+      (is (ts/make-property-key mgmt :name  String :cardinality :single))
+      (is (ts/make-property-key mgmt :type  String :cardinality :single))
+      (is (ts/make-property-key mgmt :times Long   :cardinality :single))
+      (is (ts/make-edge-label mgmt :lives   :multiplicity :many-to-one))
+      (is (ts/make-edge-label mgmt :father  :multiplicity :one-to-many))
+      (is (ts/make-edge-label mgmt :mother  :multiplicity :one-to-many))
+      (is (ts/make-edge-label mgmt :brother :multiplicity :many-to-many))
+      (is (ts/make-edge-label mgmt :pet     :multiplicity :one-to-many))
+      (is (ts/make-edge-label mgmt :battled :multiplicity :many-to-many))
+      (is (ts/build-composite-index mgmt :name-ix :vertex [:name] :unique? true))
+      (is (ts/build-composite-index mgmt :type-ix :vertex [:type]))
+      (is (ts/build-composite-index mgmt :times-ix :edge [:times]))))
+
+  (testing "Populate graph"
+    (is
+     (tg/with-transaction [tx *graph*]
+       (let [saturn   (tv/create! tx {:name "Saturn"   :type "titan"})
+             jupiter  (tv/create! tx {:name "Jupiter"  :type "god"})
+             hercules (tv/create! tx {:name "Hercules" :type "demigod"})
+             alcmene  (tv/create! tx {:name "Alcmene"  :type "human"})
+             neptune  (tv/create! tx {:name "Neptune"  :type "god"})
+             pluto    (tv/create! tx {:name "Pluto"    :type "god"})
+             sea      (tv/create! tx {:name "Sea"      :type "location"})
+             sky      (tv/create! tx {:name "Sky"      :type "location"})
+             tartarus (tv/create! tx {:name "Tartarus" :type "location"})
+             nemean   (tv/create! tx {:name "Nemean"   :type "monster"})
+             hydra    (tv/create! tx {:name "Hydra"    :type "monster"})
+             cerberus (tv/create! tx {:name "Cerberus" :type "monster"})]
+         (ted/connect! tx neptune :lives sea)
+         (ted/connect! tx jupiter :lives sky)
+         (ted/connect! tx pluto :lives tartarus)
+         (ted/connect! tx jupiter :father saturn)
+         (ted/connect! tx hercules :father jupiter)
+         (ted/connect! tx hercules :mother alcmene)
+         (ted/connect! tx jupiter :brother pluto)
+         (ted/connect! tx pluto :brother jupiter)
+         (ted/connect! tx neptune :brother pluto)
+         (ted/connect! tx pluto :brother neptune)
+         (ted/connect! tx jupiter :brother neptune)
+         (ted/connect! tx neptune :brother jupiter)
+         (ted/connect! tx cerberus :lives tartarus)
+         (ted/connect! tx pluto :pet cerberus)
+         (ted/connect! tx hercules :battled nemean   {:times 1})
+         (ted/connect! tx hercules :battled hydra    {:times 2})
+         (ted/connect! tx hercules :battled cerberus {:times 12})
+         true))))
+
+  (testing "Query graph"
     (tg/with-transaction [tx *graph*]
-        (tt/defkey-once tx :vertex-id Long {:unique? true}))
-
-    (testing "Without retries"
-      (let [random-long (long (rand-int 100000))
-            f1 (future (tg/with-transaction [tx *graph*] (tv/upsert! tx :vertex-id {:vertex-id random-long})))
-            f2 (future (tg/with-transaction [tx *graph*] (tv/upsert! tx :vertex-id {:vertex-id random-long})))]
-
-        (is (thrown? java.util.concurrent.ExecutionException
-                     (do @f1 @f2)) "The futures throw errors.")))
-
-    (testing "With retries"
-      (let [random-long (long (rand-int 100000))
-            f1 (future (tg/with-transaction-retry [tx *graph* :max-attempts 3 :wait-time 100]
-                           (tv/upsert! tx :vertex-id {:vertex-id random-long})))
-            f2 (future (tg/with-transaction-retry [tx *graph* :max-attempts 3 :wait-time 100]
-                           (tv/upsert! tx :vertex-id {:vertex-id random-long})))]
-        (is (= random-long
-               (tg/with-transaction [tx *graph*]
-                   (tv/get (tv/refresh tx (first @f1)) :vertex-id))
-               (tg/with-transaction [tx *graph*]
-                   (tv/get (tv/refresh tx (first @f2)) :vertex-id)))
-            "The futures have the correct values.")
-        (is (= 1 (count (tg/with-transaction [tx *graph*]
-                            (tv/find-by-kv tx :vertex-id random-long))))
-            "The graph has only one vertex with the specified vertex-id")))
-
-    (testing "With retries and an exponential backoff function"
-      (let [backoff-fn (fn [try-count] (+ (Math/pow 10 try-count) (* try-count (rand-int 100))))
-            random-long (long (rand-int 100000))
-            f1 (future (tg/with-transaction-retry [tx *graph* :max-attempts 3 :wait-time backoff-fn]
-                           (tv/upsert! tx :vertex-id {:vertex-id random-long})))
-            f2 (future (tg/with-transaction-retry [tx *graph* :max-attempts 3 :wait-time backoff-fn]
-                           (tv/upsert! tx :vertex-id {:vertex-id random-long})))]
-        (is (= random-long
-               (tg/with-transaction [tx *graph*]
-                   (tv/get (tv/refresh tx (first @f1)) :vertex-id))
-               (tg/with-transaction [tx *graph*]
-                   (tv/get (tv/refresh tx (first @f2)) :vertex-id)))
-            "The futures have the correct values.")
-        (is (= 1 (count (tg/with-transaction [tx *graph*]
-                            (tv/find-by-kv tx :vertex-id random-long))))
-            "The graph has only one vertex with the specified vertex-id")))))
+      (is (= #{"Jupiter" "Neptune" "Pluto"}
+             (set (map (fn [v] (tv/get v :name)) (tv/find-by-kv tx :type "god")))))
+      (let [jupiter (first (tv/find-by-kv tx :name "Jupiter"))]
+        (is jupiter)
+        (let [lives (ted/head-vertex (first (tv/edges-of jupiter :out :lives)))]
+          (is "Sky" (tv/get lives :name)))))))
